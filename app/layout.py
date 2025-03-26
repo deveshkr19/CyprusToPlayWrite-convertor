48700e38870a2063d66e163fb9b7ec05f1499546
deveshkr19 <deveshkr19@gmail.com>
2025-03-26 11:56:25 +0100

    Text changes

--- app/layout.py
+++ app/layout.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def setup_layout():
     st.set_page_config(page_title="Cypress to Playwright Converter", layout="wide")
-    st.title("🚀 Cypress to Playwright Converter using Gen AI")
+    st.title("Cypress to Playwright Converter using Gen AI and RAG")
 
     st.write("""
     ### 🔹 About This App
-    This tool converts **Cypress test scripts** to **Playwright** using AI and project-specific examples.
+    This tool converts **Cypress test scripts** to **Playwright** using AI and project-specific examples(RAG).
     """)
