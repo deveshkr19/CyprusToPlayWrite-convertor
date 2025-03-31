94b1d74456c8fd3158f925a5392b9d169bbfb8e9
deveshkr19 <deveshkr19@gmail.com>
2025-03-31 12:22:40 +0200

    improved main.py

--- app/uploader.py
+++ app/uploader.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def handle_upload():
-    uploaded_file = st.file_uploader("📤 Upload a Cypress test file (.js or .ts)", type=["js", "ts"])
+    uploaded_file = st.file_uploader("Upload a Cypress test file (.js or .ts)", type=["js", "ts"])
     if uploaded_file:
         code = uploaded_file.read().decode("utf-8")
-        st.subheader("📜 Original Cypress Code")
+        st.subheader("Original Cypress Code")
         st.code(code, language="javascript")
         return code, uploaded_file.name
     return None, None
